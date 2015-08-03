eef77e9f4db63fb86a850548f4c8e7ff3dbe70a4
MISUMI Masaru <deltam@gmail.com>
2015-08-03 21:34:51 +0900

    実験とその結果に沿った調整

--- src/neuro/train.clj
+++ src/neuro/train.clj
@@ -6,41 +6,41 @@
 
 (def ^:dynamic *weight-inc-val* 0.00001)
 (def ^:dynamic *learning-param* 0.00001)
+(def ^:dynamic *weight-random-diff* 0.001)
+
+(def ^:dynamic *report-period* 100)
 
 
-(defn train [init-nn dfn dataset]
+(defn train [init-nn dfn w-updater dataset terminate-f]
   (loop [cur-nn init-nn, diff (dfn init-nn dataset) , cnt 0]
-    (let [next (train-next cur-nn dataset dfn)
+    (let [next (train-next cur-nn dfn w-updater dataset)
           next-diff (dfn next dataset)]
-      (if (zero? (mod cnt 1000))
+      (if (zero? (mod cnt *report-period*))
         (println cnt " now diff: " next-diff))
-;      (if (< (Math/abs (- diff next-diff)) 0.00000000001)
-      (if (< next-diff 3)
+      (if (terminate-f diff next-diff)
         cur-nn
         (recur next, next-diff, (inc cnt))))))
 
-(defn train-next [nn dataset dfn]
-  (let [diff1 (dfn nn dataset)
-        nn2 (next-nn nn dfn dataset)
+(defn train-next [nn dfn w-updater dataset]
+  (let [nn1 (w-updater nn dfn dataset)
+        diff1 (dfn nn1 dataset)
+        nn2 (w-updater nn dfn dataset)
         diff2 (dfn nn2 dataset)]
-    (cond (<= diff1 diff2) nn
+    (cond (<= diff1 diff2) nn1
           (>  diff1 diff2) nn2)))
-
-(defn next-nn [nn dfn dataset]
-;  (weight-gradient nn dfn dataset))
-  (weight-randomize nn dfn dataset))
 
 
 
 (defn diff-fn-2class
   "2値分類の誤差関数 nn の出力層は1ニューロン"
   [nn dataset]
-  (* -1.0
-     (apply +
-            (for [{x :x, [d] :ans} dataset
-                  :let [[y] (core/nn-calc nn x)]]
-              (+ (* d (Math/log y))
-                 (* (- 1.0 d) (Math/log (- 1.0 y))))))))
+  (let [samples (count dataset)
+        diff-sum (apply +
+                        (for [{x :x, [d] :ans} dataset
+                              :let [[y] (core/nn-calc nn x)]]
+                          (+ (* d (Math/log y))
+                             (* (- 1.0 d) (Math/log (- 1.0 y))))))]
+    (/ (* -1 diff-sum) samples)))
 
 
 
@@ -71,9 +71,10 @@
 
 
 (defn- rand-add [x]
-  (let [d 0.000001]
-    (+ x
-       (gr/rand-nth [d 0 (* -1 d)]))))
+  (+ x
+     (gr/rand-nth [*weight-random-diff*
+                   0
+                   (* -1 *weight-random-diff*)])))
 
 (defn- weight-randomize
   "重みをランダムに更新する"
@@ -104,12 +105,12 @@
 
 (comment
 
-(def nn-2class (let [nn (nw/gen-nn :rand 3 4 1)]
+(def nn-2class (let [nn (nw/gen-nn :rand 3 4  1)]
                  ;; biasノードの初期値は0
                  (nw/map-weights (fn [w l i o]
                                    (if (or (and (= l 0) (= i 2))
                                            (and (= l 1) (= i 3)))
-                                     0.0
+                                      0.0
                                      w))
                                  nn)))
 
@@ -117,6 +118,16 @@
                        {:x [3 2 1] :ans [0]}
                        {:x [4 1 1] :ans [0]}
                        {:x [8 3 1] :ans [0]}
+                       {:x [1 8 1] :ans [0]}
+                       {:x [9 5 1] :ans [0]}
+                       {:x [5 2 1] :ans [0]}
+                       {:x [4 2 1] :ans [0]}
+                       {:x [3 3 1] :ans [0]}
+                       {:x [2 6 1] :ans [0]}
+                       {:x [1 8 1] :ans [0]}
+                       {:x [9 5 1] :ans [0]}
+
+
                        {:x [3 7 1] :ans [1]}
                        {:x [4 4 1] :ans [1]}
                        {:x [7 6 1] :ans [1]}
@@ -124,15 +135,35 @@
                        {:x [4 8 1] :ans [1]}
                        {:x [7 8 1] :ans [1]}
                        {:x [7 6 1] :ans [1]}
+                       {:x [4 5 1] :ans [1]}
+                       {:x [4 7 1] :ans [1]}
+                       {:x [8 5 1] :ans [1]}
+                       {:x [6 3 1] :ans [1]}
+                       {:x [4 7 1] :ans [1]}
+                       {:x [9 6 1] :ans [1]}
+                       {:x [7 4 1] :ans [1]}
                        ])
+(time
+ (def train-nn-2class
+   (train nn-2class diff-fn-2class weight-gradient traindata-2class
+          #(< (Math/abs (- %1 %2)) 0.00001)))
+ )
 
-(def train-nn-2class (train nn-2class diff-fn-2class traindata-2class))
+(time
+ (def nn2
+   (train train-nn-2class diff-fn-2class weight-randomize traindata-2class
+          (fn [_ d] (< d 0.5333))))
+)
 
+(defn nn-test [nn dataset ans ans-test]
+  (let [t (map (fn [x] (core/nn-calc nn2 x))
+               (for [t traindata-2class :when (= (:ans t) ans)]
+                 (:x t)))
+        cnt (count t)]
+    (/ (reduce (fn [r [w]] (if (ans-test w) (inc r) r)) 0.0 t)
+       cnt)))
 
-(map (fn [x] (core/nn-calc train-nn-2class x))
-     (for [t traindata-2class :when (= (:ans t) [0])]
-       (:x t)))
-(map (fn [x] (core/nn-calc train-nn-2class x))
-     (for [t traindata-2class :when (= (:ans t) [1])]
-       (:x t)))
+(nn-test nn2 traindata-2class [0] #(< % 0.5))
+(nn-test nn2 traindata-2class [1] #(> % 0.5))
+
 )
