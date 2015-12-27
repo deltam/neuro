c0c3d89c24b3246543e9a787d8abc7dd196fd917
MISUMI Masaru <deltam@gmail.com>
2015-12-28 01:08:27 +0900

    Delete unused functions, Move functions

--- src/neuro/network.clj
+++ src/neuro/network.clj
@@ -1,147 +1,56 @@
 (ns neuro.network
-  (:require [clojure.data.generators :as gr]))
+  (:require [neuro.vol :as vl])
+  (:require [neuro.layer :as ly]))
 
-(declare gen-num-vec gen-num-matrix seq-by-2-items update-at update-matrix-at)
-
-(declare map-matrix-indexed)
-(defn gen-layer
-  "NNの1層を作る"
-  [in out activation-f init]
-  {:nodes [in out]
-   :weights (map-matrix-indexed (fn [i _ w] (if (zero? i) 0 w)) ; bias項の初期値は0にする
-                                (gen-num-matrix init (inc in) out))
-   :func activation-f})
-
-(defn gen-nn
-  "多層ニューラルネットを定義する"
-  [init & layer-nodes]
-  (let [nodes (seq-by-2-items layer-nodes)]
-    (mapv (fn [[in out]] (gen-layer in out :sigmoid init))
-          nodes)))
-
-(defn stack-layer
-  "NNに層を追加する"
-  [nn layer]
-  (conj nn layer))
-
-(defn wget
-  "重みを取得する"
-  [nn layer in out]
-  (let [w-mat (:weights (nth nn layer))]
-    ((w-mat in) out)))
-
-(defn wput
-  "重みを更新する"
-  [nn layer in-node out-node w]
-  (let [cur-layer (nth nn layer)
-        w-mat (:weights cur-layer)
-        updated (update-matrix-at w-mat in-node out-node w)
-        new-layer (assoc cur-layer :weights updated)]
-    (update-at nn layer new-layer)))
-
-
-(defn mapv-indexed [f coll]
-  (vec (map-indexed f coll)))
-
-(defn map-matrix-indexed
-  [f mat]
-  (mapv-indexed (fn [c w-vec]
-                  (mapv-indexed (fn [r w] (f c r w))
-                                w-vec))
-                mat))
-
-(defn matrix-op
-  [op mat1 mat2]
-  (map-matrix-indexed (fn [i o w1]
-                        (op w1 (nth (nth mat2 i) o)))
-                      mat1))
 
 (defn map-nn
-  "重みの更新を一括して行なう
-  (f l i o w)"
   [f nn]
-  (mapv-indexed (fn [idx layer]
-                  (let [[in out] (:nodes layer)
-                        w-mat (:weights layer)]
-                    (assoc layer :weights
-                           (map-matrix-indexed (fn [i o w] (f idx i o w)) w-mat))))
-                nn))
+  (let [layers (:layer nn)]
+    (apply ly/network
+           (map-indexed (fn [l layer]
+                          (let [w-vol (:w layer)]
+                            (if (nil? w-vol)
+                              layer
+                              (assoc layer :w
+                                     (reduce (fn [v [i o]]
+                                               (let [w (vl/wget v i o)]
+                                                 (vl/wset v i o (f l i o w))))
+                                             w-vol
+                                             (for [x (range (:sx w-vol)) y (range (:sy w-vol))]
+                                               [x y]))))))
+                        layers))))
+
+(defn map-nn-bias
+  [f nn]
+  (let [layers (:layer nn)]
+    (apply ly/network
+           (map-indexed (fn [l layer]
+                          (let [bias-vol (:bias layer)]
+                            (if (nil? bias-vol)
+                              layer
+                              (assoc layer :bias
+                                     (reduce (fn [v o]
+                                               (let [b (vl/wget v 0 o)]
+                                                 (vl/wset v 0 o (f l o b))))
+                                             bias-vol
+                                             (range (:sy bias-vol)))))))
+                        layers))))
 
 
+(defn nn-put
+  [nn l i o w]
+  (let [layer (nth (:layer nn) l)
+        w-vol (:w layer)
+        w2-vol (vl/wset w-vol i o w)
+        layer2 (assoc layer :w w2-vol)]
+    (assoc nn :layer
+           (assoc (vec (:layer nn)) l layer2))))
 
-(defn- weight-init-f [init]
-  (if (= :rand init)
-    (binding [gr/*rnd* (java.util.Random. (System/currentTimeMillis))]
-      (fn [] (- (gr/double) 0.5)))
-    (if (fn? init)
-      init
-      (fn [] init))))
-
-(defn- gen-num-vec [init n]
-  (let [init-f (weight-init-f init)]
-    (apply vector
-           (repeatedly n init-f))))
-
-(defn- gen-num-matrix [init x y]
-  (gen-num-vec #(gen-num-vec init y) x))
-
-(defn- seq-by-2-items [s]
-  (map vector s (rest s)))
-
-(defn- update-at [v idx val]
-  (apply vector
-         (concat (subvec v 0 idx)
-                 [val]
-                 (subvec v (inc idx)))))
-
-(defn- update-matrix-at [mat x y val]
-  (update-at mat x
-             (update-at (mat x) y val)))
-
-
-
-
-(comment
-
-(def nn [{:node [3 2]
-          :weights [[0.0 0.0] ; bias
-                    [0.0 0.0]
-                    [0.0 0.0]
-                    [0.0 0.0]]
-          :func :sigmoid}
-         {:node [2 1]
-          :weights [[0.0]     ; bias
-                    [0.0]
-                    [0.0]]
-          :func :sigmoid}
-         ])
-
-(forward-one (first nn) [1 2 3])
-;=> [1 1]
-
-(def nn
-  [{:type :input
-    :in 2 :out 2}
-   {:type :fc
-    :in 2 :out 6
-    :w [[0 0 0 0 0 0] ; bias
-        [1 1 1 1 1 1]
-        [1 2 3 4 5 6]]}
-   {:type :sigmoid
-    :in 6 :out 6}
-   {:type :fc
-    :in 6 :out 2
-    :w [[0 0] ;bias
-        [1 2]
-        [1 2]
-        [1 2]
-        [1 2]
-        [1 2]
-        [1 2]]}
-   {:type :sigmoid
-    :in 2 :out 2}
-   {:type :fc
-    :in 2 :out 1
-    :w [[0]
-        [1]]}])
-)
+(defn nn-put-bias
+  [nn l o b]
+  (let [layer (nth (:layer nn) l)
+        b-vol (:bias layer)
+        b2-vol (vl/wset b-vol 0 o b)
+        layer2 (assoc layer :bias b2-vol)]
+    (assoc nn :layer
+           (assoc (vec (:layer nn)) l layer2))))
