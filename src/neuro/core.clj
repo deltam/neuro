efc7b71fab284e1ae6edc7324d81ec15b325b90c
MISUMI Masaru <deltam@gmail.com>
2015-12-31 16:42:25 +0900

    Remove unused funcs

--- src/neuro/core.clj
+++ src/neuro/core.clj
@@ -1,20 +1,4 @@
 (ns neuro.core
+  (:require [neuro.vol :as vl])
   (:require [neuro.layer :as ly])
-  (:require [neuro.network :as nw])
   (:require [neuro.func :as fnc]))
-
-
-(defn backprop
-  "誤差逆伝播法で重みを勾配を算出する"
-  [net in-seq train-seq]
-  (let [forwarded (ly/forward net in-seq)
-        delta-seq (map - forwarded train-seq)]
-    (ly/backward net delta-seq)))
-
-
-(defn nn-calc
-  "多層ニューラルネットの計算をする"
-  [net in-seq]
-  (let [l (last (:layer (ly/forward net in-seq)))
-        {[v] :w} (:in-vol l)]
-    v))
--- src/neuro/network.clj
+++ src/neuro/network.clj
@@ -56,63 +56,6 @@
 
 
 
-(defn map-nn
-  [f nn]
-  (let [layers (:layer nn)]
-    (apply ly/network
-           (map-indexed (fn [l layer]
-                          (let [w-vol (:w layer)]
-                            (if (nil? w-vol)
-                              layer
-                              (assoc layer :w
-                                     (reduce (fn [v [i o]]
-                                               (let [w (vl/wget v i o)]
-                                                 (vl/wset v i o (f l i o w))))
-                                             w-vol
-                                             (for [x (range (:sx w-vol)) y (range (:sy w-vol))]
-                                               [x y]))))))
-                        layers))))
-
-(defn map-nn-bias
-  [f nn]
-  (let [layers (:layer nn)]
-    (apply ly/network
-           (map-indexed (fn [l layer]
-                          (let [bias-vol (:bias layer)]
-                            (if (nil? bias-vol)
-                              layer
-                              (assoc layer :bias
-                                     (reduce (fn [v o]
-                                               (let [b (vl/wget v 0 o)]
-                                                 (vl/wset v 0 o (f l o b))))
-                                             bias-vol
-                                             (range (:sy bias-vol)))))))
-                        layers))))
-
-
-(defn nn-put
-  [nn l i o w]
-  (let [layer (nth (:layer nn) l)
-        w-vol (:w layer)
-        w2-vol (vl/wset w-vol i o w)
-        layer2 (assoc layer :w w2-vol)]
-    (assoc nn :layer
-           (assoc (vec (:layer nn)) l layer2))))
-
-(defn nn-put-bias
-  [nn l o b]
-  (let [layer (nth (:layer nn) l)
-        b-vol (:bias layer)
-        b2-vol (vl/wset b-vol 0 o b)
-        layer2 (assoc layer :bias b2-vol)]
-    (assoc nn :layer
-           (assoc (vec (:layer nn)) l layer2))))
-
-
-
-
-
-
 (comment
 
 (def net (nw/network
