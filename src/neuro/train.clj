b845e109d7919d0522e2edbe7c54f381e4c8b569
MISUMI Masaru <deltam@gmail.com>
2016-01-23 23:55:20 +0900

    Rewrite train funcs

--- src/neuro/train.clj
+++ src/neuro/train.clj
@@ -3,7 +3,6 @@
             [neuro.vol :as vl]
             [neuro.layer :as ly]
             [neuro.network :as nw]
-            [clojure.data.generators :as gr]
             [taoensso.timbre.profiling :as pl]))
 
 
@@ -15,12 +14,20 @@
 (def ^:dynamic *mini-batch-size* 10)
 (def ^:dynamic *momentum-param* 0.6)
 
-(def +learning-rate+ (atom 0.1))
+(def +learning-rate+ (atom 0.01))
 
 (def +train-err-vec+ (atom []))
 (def +test-err-vec+ (atom []))
 (def +go-next-batch+ (atom false))
 (def +now-nn+ (atom nil))
+
+
+(defn gen-train-pairs
+  "vector -> input, answer pair"
+  [in-test-vec]
+  (map vec
+       (partition 2
+                  (map vl/vol in-test-vec))))
 
 
 (defn init []
@@ -37,109 +44,52 @@
         (swap! +test-err-vec+ conj test-err)
         (reset! +now-nn+ nn))))
 
-(defn- momentum
-  "モメンタム項の計算"
-  [pre-nn cur-nn nn]
-  (nw/map-nn (fn [l i o w]
-               (let [dw ( - (nw/wget cur-nn l i o)
-                            (nw/wget pre-nn l i o))]
-                 (+ w (* *momentum-param* dw))))
-             nn))
-
-(defn train
-  "NNの学習を行なう"
-  [init-nn efn w-updater dataset testset terminate-f]
-  (loop [pre-nn init-nn, cur-nn init-nn, err (efn init-nn dataset) , epoc 0]
-    (let [next-nn (pl/p :update (w-updater cur-nn efn dataset))
-          train-err (pl/p :efn-train (efn next-nn dataset))
-          test-err (pl/p :efn-test (efn next-nn testset))]
-      (monitoring epoc train-err test-err next-nn)
-      (if (or (Double/isNaN train-err) @+go-next-batch+ (terminate-f epoc err train-err))
-        (do (reset! +go-next-batch+ false)
-            (if (Double/isNaN train-err) (println "NaN!!!"))
-            cur-nn)
-        (recur cur-nn, next-nn, train-err, (inc epoc))))))
-
-(declare weight-gradient weight-gradient-backprop)
-(defn train-sgd
-  "訓練データをシャッフルしてミニバッチ方式で学習する"
-  [init-nn efn dataset testset terminate-f]
-  (let [batch-data (partition *mini-batch-size* (shuffle dataset))]
-    (loop [idx (dec (count batch-data)), nn init-nn]
-      (if (< idx 0)
-          (do (println "train finish!!!")
-              nn)
-          (do (println "batch start " idx)
-              (recur (dec idx) (train nn efn weight-gradient-backprop (nth batch-data idx) testset terminate-f)))))))
-
-
-
-(defn err-fn-2class
-  "2値分類の誤差関数 nn の出力層は1ニューロン"
-  [nn dataset]
-  (let [samples (count dataset)
-        er (apply +
-                  (for [{x :x, [d] :ans} dataset
-                        :let [y (core/nn-calc nn (vl/vol x))]]
-                    (+ (* d (Math/log y))
-                       (* (- 1.0 d) (Math/log (- 1.0 y))))))]
-    (/ (- er) samples)))
-
-
 
 ;; 重み更新関数
 
-(defn- update-by-gradient
-  "重みを勾配に従って更新した値を返す"
-  [w grad bias?]
-  (let [de (if bias? grad
-               (+ grad (* *weight-decay-param* w)))]
-    (- w (* @+learning-rate+ de))))
+(defn w-updater
+  []
+  (fn [w dw] (- w (* @+learning-rate+ dw))))
 
+(def default-updater (w-updater))
 
-;; 数値微分による勾配降下法
+;(defn- update-by-gradient
+;  "重みを勾配に従って更新した値を返す"
+;  [w grad bias?]
+;  (let [de (if bias? grad
+;               (+ grad (* *weight-decay-param* w)))]
+;    (- w (* @+learning-rate+ de))))
 
-(defn- gradient
-  "nnの微小増分の傾きを返す"
-  [nn1 nn2 efn dataset]
-  (let [y1 (efn nn1 dataset)
-        y2 (efn nn2 dataset)]
-    (/ (- y2 y1) *weight-inc-val*)))
-
-(defn weight-gradient
-  "勾配降下法で重みを更新する"
-  [nn efn dataset]
-  (let [nn-w (ly/map-nn (fn [l i o w]
-                          (let [nn2 (ly/nn-put nn l i o (+ w *weight-inc-val*))]
-                            (pl/p :gradient
-                                  (let [grad (gradient nn nn2 efn dataset)]
-                                    (update-by-gradient w grad false)))))
-                        nn)]
-    (ly/map-nn-bias (fn [l o b]
-                      (let [nn2 (ly/nn-put-bias nn-w l o (+ b *weight-inc-val*))]
-                        (pl/p :gradient
-                              (let [grad (gradient nn-w nn2 efn dataset)]
-                                (update-by-gradient b grad true)))))
-                    nn-w)))
+;(defn- momentum
+;  "モメンタム項の計算"
+;  [pre-nn cur-nn nn]
+;  (nw/map-nn (fn [l i o w]
+;               (let [dw ( - (nw/wget cur-nn l i o)
+;                            (nw/wget pre-nn l i o))]
+;                 (+ w (* *momentum-param* dw))))
+;             nn))
 
 
 
-;; 誤差逆伝播法による勾配降下法
+;; train funcs
 
-(defn weight-gradient-backprop
-  "勾配降下法で重みを更新する"
-  [nn efn dataset]
-  (let [samples (count dataset)
-        d-mat-seq (map (fn [{in-seq :x, train-seq :ans}]
-                         (core/backprop nn in-seq train-seq))
-                       dataset)
-        d-mat-sum (reduce (fn [r mat-seq]
-                            (vec (map (fn [m1 m2] (nw/matrix-op + m1 m2))
-                                      r mat-seq)))
-                          (first d-mat-seq) (rest d-mat-seq))
-        d-mat (mapv #(nw/map-matrix-indexed (fn [_ _ w] (/ w samples)) %)
-                    d-mat-sum)]
-    (nw/map-nn (fn [l i o w]
-                 (let [grad (-> d-mat (nth l) (nth i) (nth o))]
-                   (pl/p :gradient (update-by-gradient w grad (zero? i)))))
-             nn)))
+(defn train-seq
+  [net train-pairs]
+  (nw/backprop-n-seq net train-pairs default-updater))
+
+(defn train-one
+  [t-seq terminate-f]
+  (loop [cur (second t-seq), s (rest (rest t-seq)), loss 0.0, epoc 0]
+    (monitoring epoc loss 0.0 cur)
+    (if (terminate-f (nw/loss cur))
+      cur
+      (recur (first s) (rest s) (nw/loss cur) (inc epoc)))))
+
+(defn train
+  [net train-pairs test-pairs terminate-f]
+  (let [batchs (partition *mini-batch-size* (shuffle train-pairs))]
+    (loop [cur net, b (first batchs), r (rest batchs)]
+      (if (nil? b)
+        cur
+        (recur (train-one (train-seq cur b) terminate-f)
+               (first r) (rest r))))))
