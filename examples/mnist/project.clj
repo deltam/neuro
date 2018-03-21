d8308af6814215db3d734916ad6343153331322e
MISUMI Masaru <deltam@gmail.com>
2018-03-21 15:29:33 +0900

    Update version for clojure, plugins
    Update to clojure 1.8.0 because lein-gorilla is broken on clojure 1.9.0
    https://github.com/JonyEpsilon/lein-gorilla/issues/10

--- examples/mnist/project.clj
+++ examples/mnist/project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-            [lein-gorilla "0.3.4"]]
+  :plugins [[cider/cider-nrepl "0.16.0"]
+            [lein-gorilla "0.4.0"]]
   :source-paths ["src" "../../src"]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.generators "0.1.2"]
                  [com.taoensso/timbre "4.1.1"]])
--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-            [lein-gorilla "0.3.4"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :plugins [[cider/cider-nrepl "0.16.0"]
+            [lein-gorilla "0.4.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.generators "0.1.2"]
                  [com.taoensso/timbre "4.1.1"]])
