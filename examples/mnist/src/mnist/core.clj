34a84b2e709e257e885351de12038150260284c4
MISUMI Masaru <deltam@users.noreply.github.com>
2018-08-07 09:23:07 +0900

    Show approximate time of next epoch

--- examples/mnist/src/mnist/core.clj
+++ examples/mnist/src/mnist/core.clj
@@ -22,21 +22,38 @@
                 (md/vol->digit (nw/feedforward net img-vol))))
            test-pairs)))
 
+(def start-time-now-epoch (atom 0))
+
 (defn report [ep net]
   (let [ok (evaluate net test-data)
-        n (count test-data)]
-    (printf "epoch %d: %d / %d\n" ep ok n)
+        n (count test-data)
+        elapsed (- (System/currentTimeMillis) @start-time-now-epoch)]
+    (printf "epoch %d: %d / %d  (%4.2f min)\n" ep ok n (float (/ elapsed 60000.0)))
     (flush)
-;    (sh "say" (str "epoc " ep " " (float (* 100 (/ ok n))))) ; for mac user
+    (reset! start-time-now-epoch (System/currentTimeMillis))
+    (sh "say" (str "epoc " ep " " (float (* 100 (/ ok n))))) ; for mac user
     ))
+
 
 (defn train
   ([] (train net))
   ([net]
+   (reset! start-time-now-epoch (System/currentTimeMillis))
    (nt/init)
    (nt/sgd net
            train-data
-           :mini-batch-size 10
+           :mini-batch-size 20
            :epoch-limit 30
-           :learning-rate 3.0
+           :learning-rate 1.0
            :epoch-reporter report)))
+
+
+(defn print-time-to-next-epoch []
+  (let [done-batchs (mod (count @nt/+train-loss-history+) @nt/+num-batchs+)
+        now-elapsed (- (System/currentTimeMillis) @start-time-now-epoch)
+        msec-per-batch (if (zero? done-batchs)
+                         now-elapsed
+                         (/ now-elapsed done-batchs))
+        until-msec (* (- @nt/+num-batchs+ done-batchs) msec-per-batch)]
+    (printf "start next epoch at %4.2f min later\n"
+            (float (/ until-msec 60000)))))
