08cb8348355ff7b68656af1897f41d6514ec318e
MISUMI Masaru <deltam@users.noreply.github.com>
2018-08-10 13:37:31 +0900

    Added exported funcs

--- src/neuro/core.clj
+++ src/neuro/core.clj
@@ -1,4 +1,17 @@
 (ns neuro.core
-  (:require [neuro.vol :as vl])
-  (:require [neuro.layer :as ly])
-  (:require [neuro.func :as fnc]))
+  (:require [neuro.network :as nw]
+            [neuro.train :as tr]))
+
+;; copy from funcool/octet.util
+(defmacro ^:private defalias
+  [sym sym2]
+  `(do
+     (def ~sym (var ~sym2))
+     (alter-meta! (var ~sym) merge (dissoc (meta (var ~sym2)) :name))))
+
+
+(defalias gen-net nw/gen-net)
+(defalias feedforward nw/feedforward)
+(defalias with-params tr/with-params)
+(defalias init tr/init)
+(defalias sgd tr/sgd)
