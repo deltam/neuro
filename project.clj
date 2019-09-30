396b792bd648720442784be74e0d1819287fd8ef
MISUMI Masaru <deltam@users.noreply.github.com>
2019-09-30 22:32:02 +0900

    v0.1.1-SNAPSHOT -> v0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deltam/neuro "0.1.1-SNAPSHOT"
+(defproject deltam/neuro "0.2.0-SNAPSHOT"
   :description "Deep Neural Network written in Clojure from scratch"
   :url "https://github.com/deltam/neuro"
   :license {:name "MIT License"
